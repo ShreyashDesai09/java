eda16fbf7f7f9442f118d2d9e4621509bf8167ec
Shreyash <shreyashdesai31796@gmail.com>
2025-07-27 20:47:35 +0530

    1

---
+++ 1_class_and_objects.java
@@ -0,0 +1,12 @@
+class Car {
+    string color;
+    void driver(){
+        System.out.println("Car is Driving")
+    }
+}
+
+public class Main {
+    public static void main(string[] args) {
+        Car
+    }
+}
