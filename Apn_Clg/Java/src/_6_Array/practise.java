88a66dab0862569578961a3002264addd44c7ef9
Shreyash <shreyashdesai31796@gmail.com>
2025-08-04 11:35:49 +0530

    04-08-2025

---
+++ Apn_Clg/Java/src/_6_Array/practise.java
@@ -0,0 +1,17 @@
+package _6_Array;
+
+import java.util.*;
+
+public class practise{
+	public static void main(String[] args){
+		
+
+		// 1 Check Maximum and Minimum Number in Array
+		
+
+		// 2 Check if Array is in Ascending Order or not
+
+
+	}
+}
+
