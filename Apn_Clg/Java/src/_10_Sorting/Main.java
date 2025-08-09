6899173f17d2330264b28f1e2a915004d8453d9e
ShreyashDesai09 <shreyashdesai31796@gmail.com>
2025-08-09 21:14:53 +0530

    09-08-2025

--- Apn_Clg/Java/src/_10_Sorting/Main.java
+++ Apn_Clg/Java/src/_10_Sorting/Main.java
@@ -10,7 +10,7 @@ public class Main{
     }
     public static void main(String[] args){
 
-        //Bubble Sort
+        //BUBBLE SORT
         int arr[] = {4,8,9,10,3};
 
         for(int i = 0 ; i < arr.length - 1 ; i++){
@@ -22,11 +22,24 @@ public class Main{
                 }
             }
         }
-
+        System.out.println("BUBBLE SORT");
         printArray(arr);
 
-
-
+        int x[] = {3,1,6,13,25,7,10};
+        //SELECTION SORT
+        for(int a = 0 ; a < x.length - 1 ; a++) {
+            int smallest = a;
+            for(int b = a + 1 ; b < x.length ; b++) {
+                if(x[smallest] > x[b]) {
+                    smallest = b;
+                }
+            }
+            int temp = x[smallest];
+            x[smallest] = x[a];
+            x[a] = temp;
+        }
+        System.out.println("\nSELECTION SORT");
+        printArray(x);
 
     }
 }
